4f483a38fb42695e4be0af6e9406d6f19f8a2305
Kunark Khewal <kunark.khewal88@gmail.com>
2022-05-01 01:09:53 +0530

    tailwind applied tested and added vue router and tried navigation guard

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  }
+]
+const isLogin = false;
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !isLogin) next('/login')
+  else if (to.name == 'Login' && isLogin) next('/login')
+  else next()
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-4xl text-red-500">
+    <h1>This is an login page</h1>
+  </div>
+</template>
