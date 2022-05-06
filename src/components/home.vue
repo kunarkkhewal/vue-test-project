60f9181afc29cc95fd9db578cde74db147f76439
Kunark Khewal <kunark.khewal88@gmail.com>
2022-05-07 01:00:37 +0530

    setup vuex and worked on state and getters

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="bg-green-200 border rounded-lg">Home Page</div>
+  <div>
+    <div class="bg-green-200 border rounded-lg">Home Page</div>
+    <p>{{ $store.state.count }}</p>
+    <button class="border rounded-sm mt-2 px-2" @click="increment">increment</button>
+    <p>done todo count - {{doneTodosCount}}</p>
+  </div>
 </template>
 
 <script>
+// import store from '@/store'
 export default {
-  name: 'home-page'
+  name: 'home-page',
+  // store,
+  methods: {
+    increment () {
+      console.log(' ------- in here')
+      this.$store.commit('increment');
+    }
+  },
+  computed: {
+    doneTodosCount () {
+      return this.$store.getters.doneTodos.length
+    }
+  }
 }
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
+  },
+  getters: {
+    doneTodos (state) {
+      return state.todos.filter(todo => todo.done)
+    }
+  },
+  mutations: {
+    increment (state) {
+      ++state.count
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
